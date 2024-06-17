5d995d827995083504121843bcb8b0e7f88748bb
Sanjeev <sk3616593@gmail.com>
2024-06-17 16:06:08 +0530

    some changes on news pics

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -3,48 +3,44 @@ import bhaskar from "../assets/bhaskar.png";
 import dailyhunt from "../assets/dailyhunt.png";
 import hindustan from "../assets/hindustan.png";
 import news18 from "../assets/new18.png";
+
 function Company() {
   return (
-    <div
-      className="h-[477.53px] flex flex-col justify-center items-center"
-      style={{
-        padding: "100px 84px",
-        gap: "59px",
-        // Added width to match your original requirement
-      }}
-    >
-      <div className="flex flex-col items-center justify-center h-[164px] w-[1272px] text-center p-10">
-        <h2
-          className="text-4xl mb-4 font-bold leading-tight"
-          style={{
-            background: "linear-gradient(to right, #53ADE4, #277BA8)",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-            fontSize: "36px",
-          }}
-        >
-          Big companies are here
-        </h2>
-        <p className="text-center max-w-[850px] h-[86px] text-[#42526B] text-[20px] ">
-          With lots of unique blocks, you can easily build a page without
-          coding. Build your next <br></br> consultancy website within a few
-          minutes. You have a business to run. Stop <br></br> worrying about
-          cross-browser bugs, designing new pages.
-        </p>
-      </div>
+    <div className="py-10 px-4 md:px-8 lg:px-16 xl:px-24">
+      <div className="max-w-[1272px] mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h2
+            className="text-4xl mb-4 font-bold leading-tight text-center"
+            style={{
+              background: "linear-gradient(to right, #53ADE4, #277BA8)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+              fontSize: "36px",
+            }}
+          >
+            Big companies are here
+          </h2>
+          <p className="max-w-[850px] text-[#42526B] text-lg text-center mb-10">
+            With lots of unique blocks, you can easily build a page without
+            coding. Build your next consultancy website within a few minutes.
+            You have a business to run. Stop worrying about cross-browser bugs,
+            designing new pages.
+          </p>
+        </div>
 
-      <div className="h-[54.53px] w-[1272px] flex justify-around items-center justify-center">
-        <div className="h-[54.3px]">
-          <img src={bhaskar} alt="" />
-        </div>
-        <div>
-          <img src={hindustan} alt="" />
-        </div>
-        <div>
-          <img src={news18} alt="" />
-        </div>
-        <div>
-          <img src={dailyhunt} alt="" />
+        <div className="flex flex-wrap justify-center gap-4 md:justify-around">
+          <div className="w-[139px] h-[54.53px] mt-[-13px]">
+            <img src={bhaskar} alt="" className="h-full" />
+          </div>
+          <div className="h-[49.76px] w-[187.56px]">
+            <img src={hindustan} alt="" className="h-full" />
+          </div>
+          <div className="h-[47px] w-[94px]">
+            <img src={news18} alt="" className="h-full" />
+          </div>
+          <div className="h-[42px] w-[160px]">
+            <img src={dailyhunt} alt="" className="h-full" />
+          </div>
         </div>
       </div>
     </div>
