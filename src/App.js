dcb33fe48cd6d1163a4e0eab0a7ebe846ae53981
Sanjeev <sk3616593@gmail.com>
2024-06-18 11:35:05 +0530

    Features component done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Hero from './components/Hero';
 import Company from './components/Company';
 import Courses from './components/Courses';
+import Features from './components/Features';
+import Testimonial from './components/Testimonial';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Hero/>
       <Company/>
       <Courses/>
+      <Features/>
+      <Testimonial/>
     </>
   );
 }
